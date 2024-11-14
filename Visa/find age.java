16d252eddd462781620f56cd514657d9b1089c85
harani-36 <kkanumaharani@gmail.com>
2024-11-14 21:01:28 +0530

    Create find age.java

---
+++ Visa/find age.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x=sc.nextInt();
+        System.out.println(10+(x-25));
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
