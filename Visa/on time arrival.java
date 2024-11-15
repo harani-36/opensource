8d19f8867ae113e5641fc9a89cfa99ec56a61ae6
harani-36 <kkanumaharani@gmail.com>
2024-11-15 10:52:12 +0530

    Create on time arrival.java

---
+++ Visa/on time arrival.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++){
+            int x=sc.nextInt();
+            if(x<30){
+                System.out.println("NO");
+            }else{
+                System.out.println("YES");
+            }
+        }
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
