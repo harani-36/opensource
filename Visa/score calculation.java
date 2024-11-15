f8f8c7f59363d47864ff3b13a8979c10b5f516fc
harani-36 <kkanumaharani@gmail.com>
2024-11-15 10:48:03 +0530

    Create score calculation.java

---
+++ Visa/score calculation.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++){
+            int x=sc.nextInt();
+            int n=sc.nextInt();
+            System.out.println(n*(x/10));
+        }
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
